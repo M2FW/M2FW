dae69ed7a56a5b7f9242d01eabddb707205ecc8c
Jay Lee <jaylee.possible@gmail.com>
2020-12-12 05:52:14 +0900

    playground-cli

--- packages/playground-cli/src/bootstrap.ts
+++ packages/playground-cli/src/bootstrap.ts
@@ -1,6 +1,21 @@
-console.log('Start bootstrapping')
+import { addPages, route } from '@m2fw/router'
 
-console.log('Bootstrapping is done.')
+import { store } from '@m2fw/redux-manager'
+
+store.addReducers({
+  route: route as any,
+})
+
+const pages = [
+  {
+    title: 'Menu Manager',
+    tagName: 'menu-management',
+    route: 'menus',
+    importer: async () => console.log('sample'),
+  },
+]
+debugger
+addPages(pages)
 
 import('./playground-app').then(() => {
   console.log(`playground-cli is launched! (Powered by M2FW)`)
