6882ea847fa3c5e51a3c81197159ba73473bf7b6
Jay Lee <jaylee.possible@gmail.com>
2021-10-28 18:31:09 +0900

    [tag-iframe] Append inner html in connectedCallback

--- packages/tag-iframe/src/components/TagIframe.ts
+++ packages/tag-iframe/src/components/TagIframe.ts
@@ -1,13 +1,13 @@
 export class TagIframe extends HTMLIFrameElement {
-  constructor() {
-    super()
-    this.addEventListener('load', this.onLoadHandler)
-  }
-
-  onLoadHandler(): void {
+  connectedCallback() {
     if (!this.contentDocument?.body) return
     this.contentDocument.body.innerHTML = this.textContent || ''
     this.textContent = ''
+  }
+
+  setContent(content: string): void {
+    if (!this.contentDocument?.body) throw new Error('Content document body is not loaded yet')
+    this.contentDocument.body.innerHTML = content
   }
 }
 
